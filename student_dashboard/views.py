dcc24ac801dc31324947b9edded9aa4f42a9b898
sagar <sagar.vidhya816@gmail.com>
2017-01-22 19:57:43 +0530

    first commit

---
+++ student_dashboard/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+def index(request):
+    return render(request, 'student_dashboard/index.html')
+def indexcn(request):
+    return render(request, 'student_dashboard/index_cn.html')
+def home(request):
+    return render(request, 'student_dashboard/home.html')
+def calendar(request):
+    return render(request, 'student_dashboard/calendar.html')
+def chartjs(request):
+    return render(request, 'student_dashboard/chartjs.html')
+def chartjs2(request):
+    return render(request, 'student_dashboard/chartjs2.html')
+def contacts(request):
+    return render(request, 'student_dashboard/contacts.html')
+def ecommerce(request):
+    return render(request, 'student_dashboard/e_commerce.html')
+def echarts(request):
+    return render(request, 'student_dashboard/echarts.html')
+def fixedfooter(request):
+    return render(request, 'student_dashboard/fixed_footer.html')
+def fixedsidebar(request):
+    return render(request, 'student_dashboard/fixed_sidebar.html')
+def form(request):
+    return render(request, 'student_dashboard/form.html')
+def formadvanced(request):
+    return render(request, 'student_dashboard/form_advanced.html')
+def formbuttons(request):
+    return render(request, 'student_dashboard/form_buttons.html')
+def formupload(request):
+    return render(request, 'student_dashboard/form_upload.html')
+def formvalidation(request):
+    return render(request, 'student_dashboard/form_validation.html')
+def formwizards(request):
+    return render(request, 'student_dashboard/form_wizards.html')
+def generalelements(request):
+    return render(request, 'student_dashboard/general_elements.html')
+def glyphicons(request):
+    return render(request, 'student_dashboard/glyphicons.html')
+def icons(request):
+    return render(request, 'student_dashboard/icons.html')
+def inbox(request):
+    return render(request, 'student_dashboard/inbox.html')
+def index2(request):
+    return render(request, 'student_dashboard/index2.html')
+def index3(request):
+    return render(request, 'student_dashboard/index3.html')
+def invoice(request):
+    return render(request, 'student_dashboard/invoice.html')
+def tables(request):
+    return render(request, 'student_dashboard/tables.html')
+def tabledynamic(request):
+    return render(request, 'student_dashboard/tables_dynamic.html')
+def mediagallery(request):
+    return render(request, 'student_dashboard/media_gallery.html')
+def typography(request):
+    return render(request, 'student_dashboard/typography.html')
+def widgets(request):
+    return render(request, 'student_dashboard/widgets.html')
+def error403(request):
+    return render(request, 'student_dashboard/page_403.html')
+def error404(request):
+    return render(request, 'student_dashboard/page_404.html')
+def error500(request):
+    return render(request, 'student_dashboard/page_500.html')
+def pricingtables(request):
+    return render(request, 'student_dashboard/pricing_tables.html')
+def login(request):
+    return render(request, 'student_dashboard/login.html')
+def plainpage(request):
+    return render(request, 'student_dashboard/plain_page.html')
+def morisjs(request):
+    return render(request, 'student_dashboard/morisjs.html')
+def othercharts(request):
+    return render(request, 'student_dashboard/other_charts.html')
+def projects(request):
+    return render(request, 'student_dashboard/projects.html')
+def projectdetail(request):
+    return render(request, 'student_dashboard/project_detail.html')
+def profile(request):
+    return render(request, 'student_dashboard/profile.html')
+def level2(request):
+    return render(request, 'student_dashboard/level2.html')
+
+
+
+
